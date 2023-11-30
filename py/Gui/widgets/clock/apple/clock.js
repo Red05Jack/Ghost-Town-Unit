c6582b67b308de3797705ade7e666a72c3fd63c3
Red__Jack <jakob.wurzrainer@kaffeepause.net>
2023-11-30 19:40:58 +0100

    V0.1.8
    - Hinzufügen des Eigenen Fonts.
    - Weiterarbeiten an der Clock.js.

--- py/Gui/widgets/clock/apple/clock.js
+++ py/Gui/widgets/clock/apple/clock.js
@@ -1,5 +1,42 @@
 // clock.js
 
+
+// Randomizer
+function getRandomNumber(min, max) {
+  // Generate a random number between 0 and 1
+  const randomFraction = Math.random();
+
+  // Scale the random number to fit the desired range
+  const randomNumber = min + randomFraction * (max - min);
+
+  // Use Math.floor to round down to the nearest integer
+  return Math.floor(randomNumber);
+}
+
+
+//
+function randomiz(element) {
+
+  const fontSize = getRandomNumber(95, 105);
+  const marginRight = getRandomNumber(0, 8);
+  const color = "rgba(0, 128, 255, 0.7)";
+  const rotation = getRandomNumber(-8, 8);
+
+  console.log(fontSize);
+    console.log(marginRight);
+      console.log(color);
+        console.log(rotation);
+
+  hoursDigitOne.style.fontSize = fontSize + "px";
+  hoursDigitOne.style.marginRight = "-" + marginRight + "px";
+  hoursDigitOne.style.color = color
+  hoursDigitOne.style.transform = "rotate(" + rotation + "deg)";
+
+  // Output to the Console
+  console.log("Test");
+}
+
+
 // Function to print the current time on the clock
 function clockPrint() {
   // Get the current time and convert it into separate variables for hours and minutes
@@ -22,12 +59,15 @@ function clockPrint() {
   document.getElementById("minutesDigitOne").innerHTML = minutesDigit[0];
   document.getElementById("minutesDigitTwo").innerHTML = minutesDigit[1];
 
+  // Randomiz the Digits
+  randomiz();
+
   // Output to the Console
   console.log("Current time: " + hours + ":" + minutes);
 }
 
 // Update the clock every second
-setInterval(clockPrint, 1000);
+setInterval(clockPrint, 60000);
 
 // Initial call to display the current time immediately
 clockPrint();
