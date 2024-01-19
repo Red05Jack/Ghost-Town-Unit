b52e23dfc9dd6d4eef0bf4d9237ae35213cf1b2e
Red__Jack <jakob.wurzrainer@kaffeepause.net>
2024-01-19 11:57:39 +0100

    V0.1.16

---
+++ py/Gui/widgets/music/small/controlls.js
@@ -0,0 +1,20 @@
+// Project: Ghost Town Unit
+// Author: Jakob Wurzrainer
+// Copyright (c) 2024:
+//   Ghost Town
+//   Kaffeepause.net
+
+function PlayForward() {
+  console.log("Play forward button pressed");
+
+}
+
+function Pouse() {
+  console.log("Pouse button pressed");
+
+}
+
+function PlayBackward() {
+  console.log("Play backward button pressed");
+
+}
