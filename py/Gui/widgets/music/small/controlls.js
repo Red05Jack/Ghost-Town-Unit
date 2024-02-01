de2e1652b32f7769348765d4332cabfde0025bef
Red__Jack <jakob.wurzrainer@kaffeepause.net>
2024-02-01 08:42:28 +0100

    V0.1.17

--- py/Gui/widgets/music/small/controlls.js
+++ py/Gui/widgets/music/small/controlls.js
@@ -7,9 +7,17 @@
 function PlayForward() {
   console.log("Play forward button pressed");
 
+
+  const element = document.getElementById('arrow1');
+  element.classList.add('animate1');
+
+  // Optional: Entfernt die Klasse nach der Animation, damit sie erneut ausgelöst werden kann
+  element.addEventListener('animationend', () => {
+    element.classList.remove('animate1');
+  });
 }
 
-function Pouse() {
+function PlayPouse() {
   console.log("Pouse button pressed");
 
 }
